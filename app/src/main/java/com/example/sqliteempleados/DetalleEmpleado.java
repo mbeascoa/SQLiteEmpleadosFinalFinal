7d9167782bbe8b6ee78132128b9824fb42e85837
MIGUEL BEASCOA <77923795+mbeascoa@users.noreply.github.com>
2021-07-06 20:33:10 +0200

    versiontrabajo en casa

---
+++ app/src/main/java/com/example/sqliteempleados/DetalleEmpleado.java
@@ -0,0 +1,50 @@
+package com.example.sqliteempleados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleEmpleado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_empleado);
+        Sql usdbh = new Sql(this, "Empleados", null, 1);
+        SQLiteDatabase db = usdbh.getWritableDatabase();
+        borrarDato(db);
+        db.close();
+        // actualizaDato(sqlDB);
+        // insertarNuevo(sqlDB);
+        // concultardatos(sqlDB);
+        // borrarTabla();
+        // borrarDato(sqlDB);
+    }
+    private void borrarDato(SQLiteDatabase db) {
+        String tabla = "Empleados";
+        String whereClause = "cod=?";
+        String[] whereArgs = new String[] {"6"};
+        db.delete(tabla, whereClause, whereArgs);
+    }
+    private void actualizaDato(SQLiteDatabase db) {
+        ContentValues actualizaReg = new ContentValues();
+        actualizaReg.put("nombre","usuario6");
+        //Actualizamos el registroenla base de datos
+        db.update("Usuarios", actualizaReg, "cod=6", null);
+    }
+
+    private void borrarTabla() {
+        Sql usdbh = new Sql(this, "Empleados", null, 1);
+        SQLiteDatabase db = usdbh.getWritableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS Empleados");
+        db.close();
+    }
+    private void consultarDatos(SQLiteDatabase db) {
+        String[] args = new String[] {"usuario6"};
+        String sql = "Select * from Usuarios where nombre =?";
+        Cursor c = db.rawQuery(sql, args);
+        int idIndex = c.getColumnIndexOrThrow("cod");
+        ArrayList<String>miArrayList = new ArrayList<String>();
+        while(c.moveToNext()) {
+            miArrayList.add(c.getString(1)); // ó idIndex
+        }
