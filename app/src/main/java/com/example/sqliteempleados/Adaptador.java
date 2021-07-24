13d8211e11aa28b7754f4efc6843bbd19651be48
mbeascoa <miguel.beascoabeastek.net>
2021-07-24 15:06:07 +0200

    quinta

--- app/src/main/java/com/example/sqliteempleados/Adaptador.java
+++ app/src/main/java/com/example/sqliteempleados/Adaptador.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder>  {
 
-
     private List<Empleados> listaEmpleados;
 
     public Adaptador(List<Empleados> ListaEmpleado) {
@@ -30,6 +29,13 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder>  {
     public void onBindViewHolder(ViewHolder holder, int position) {
         String nombre = listaEmpleados.get(position).getNombre();
         holder.txtnombre.setText(nombre);
+        String apellido = listaEmpleados.get(position).getApellido();
+        holder.txtapellido.setText(apellido);
+        String oficio = listaEmpleados.get(position).getOficio();
+        holder.txtoficio.setText(oficio);
+        int salario = listaEmpleados.get(position).getSalario();
+        holder.txtsalario.setText(String.valueOf(salario));
+
     }
 
     @Override
@@ -38,10 +44,14 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder>  {
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView txtnombre;
+        private TextView txtnombre, txtapellido, txtoficio, txtsalario;
         public ViewHolder(View v) {
             super(v);
             txtnombre = (TextView) v.findViewById(R.id.txtNombre);
+            txtapellido=(TextView) v.findViewById(R.id.txtapellido);
+            txtoficio=(TextView) v.findViewById(R.id.txtoficio);
+            txtsalario=(TextView) v.findViewById(R.id.txtsalario);
+
         }
     }
 
